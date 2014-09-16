84f41636261c93124510a1d7f9120fcb85646d84
LabKitty <Shepard1>
2014-09-16 09:10:21 +0300

    Add file

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>harjutus-2 - PHP põhitõed</title>
+</head>
+
+<body>
+    <h1>See dokument on minu esimene katsetus PHP-ga</h1>
+	<?php 
+		echo "Sinu teksti esimene pool" . " ja sinu teksti teine pool.";
+		echo "<br>";
+		echo "Sinu teksti esimene pool";
+	?>
+	
+		<br>
+		<h2> Arvutamine </h2>
+		<p> Siin teeme erinevaid tehteid.</p>
+		<?php echo "5.5" + "4.7" ?>
+</body>
+</html>
